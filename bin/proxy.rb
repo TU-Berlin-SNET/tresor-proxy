5cc3cad9c2bed805e4c130d1a076f2354d5baf61
Mathias Slawik <mathias.slawik@tu-berlin.de>
2013-10-15 11:38:50 +0200

    Added options to proxy.rb

--- bin/proxy.rb
+++ bin/proxy.rb
@@ -1,25 +1,37 @@
 require_relative '../lib/tresor_proxy'
 
 require 'eventmachine'
-require 'ruby-prof'
+require 'slop'
 
-EventMachine.threadpool_size = 8
+opts = Slop.parse do
+  banner 'Usage: proxy.rb [options]'
 
-proxy = Tresor::TresorProxy.new('127.0.0.1', '3001')
+  on 'i', 'ip', 'The ip address to bind to (default: all)'
+  on 'p', 'port', 'The port number (default: 80)'
+  on 't', 'trace', 'Enable tracing'
+end
+
+proxy = Tresor::TresorProxy.new(opts[:ip] || '127.0.0.1', opts[:port] || '80')
+
+require 'ruby-prof' if opts.trace?
 
 Thread.new do
-  RubyProf.start
+  RubyProf.start if opts.trace?
 
   proxy.start
 end
+
+puts 'Press any key to exit'
 
 readline
 
 proxy.stop
 
-result = RubyProf.stop
+if opts.trace?
+  result = RubyProf.stop
 
-printer = RubyProf::CallTreePrinter.new(result)
-File.open('profile-results', 'w') do |f|
-  printer.print(f)
+  printer = RubyProf::CallTreePrinter.new(result)
+  File.open('profile-results', 'w') do |f|
+    printer.print(f)
+  end
 end
