d0358898901fd1a253bac639791ab8e603cf51a1
Mathias Slawik <mathias.slawik@tu-berlin.de>
2014-08-26 13:06:58 +0200

    Fix: Help option overwrote hostname option

--- bin/proxy.rb
+++ bin/proxy.rb
@@ -11,7 +11,7 @@ opts = Slop.parse(ARGV, :help => true) do
 
   on 'i=', 'ip', 'The ip address to bind to (default: all)'
   on 'p=', 'port', 'The port number (default: 80)'
-  on 'h=', 'hostname', 'The HTTP hostname of the proxy'
+  on 'n=', 'hostname', 'The HTTP hostname of the proxy'
   on 'P=', 'threadpool', 'The Eventmachine thread pool size (default: 20)'
   on 't', 'trace', 'Enable tracing'
   on 'l=', 'loglevel', 'Specify log level (FATAL, ERROR, WARN, INFO, DEBUG - default INFO)'
