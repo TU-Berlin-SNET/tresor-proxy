df325c66bd41cf51f136293d5cb0679b7cbca905
Mathias Slawik <mathias.slawik@tu-berlin.de>
2014-12-04 20:21:07 +0100

    Finalized SkIDentity integration

--- lib/tresor/frontend/claim_sso/claim_sso_security_token.rb
+++ lib/tresor/frontend/claim_sso/claim_sso_security_token.rb
@@ -31,7 +31,23 @@ module Tresor
         end
 
         def attributes_hash
-          Hash[attributes.map {|a| [a['Name'], a.children.map(&:text)]}]
+          attribute_statements = Hash[attributes.map {|a| [a['Name'], a.children.map(&:text)]}]
+
+          userdata = attribute_statements["http://schemas.microsoft.com/ws/2008/06/identity/claims/userdata"]
+
+          if userdata
+            skidentity_attributes = Hash[userdata.map { |data| [data.split(':')[0..-2].join(':'), [data.split(':')[-1]]] }]
+
+            attribute_statements.merge!(skidentity_attributes)
+
+            eid = skidentity_attributes['http://www.skidentity.de/att/eIdentifier']
+
+            %w[hpc-id npa-id egk-id].each do |id|
+              attribute_statements["http://schemas.cloud-tresor.com/schema/2014/11/#{id}"] = [eid]
+            end
+          end
+
+          attribute_statements
         end
       end
     end
